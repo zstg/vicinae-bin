effd2ac4b6b2e9a3e46ee1a2ff65ef6de36cc769
zstg <o0vckutt@duck.com>
2025-10-08 13:08:08 +0530

    Changes

--- flake.nix
+++ flake.nix
@@ -1,116 +1,23 @@
 {
-  description = "Vicinae-bin flake with git and stable outputs";
+  description = "vicinae-bin flake delegating to vicinae’s flake";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    home-manager.url = "github:nix-community/home-manager";
-    vicinae-src = {
-      url = "github:vicinaehq/vicinae"; # Vicinae repository for git source
-      # For stable tarball version we use fetchTarball in package definition below
-    };
+    git.url = "github:vicinaehq/vicinae";
+    stable.url = "github:vicinaehq/vicinae?ref=v0.14.2";
   };
 
-  outputs = { self, nixpkgs, home-manager, vicinae-src, ... }@inputs:
-    let
-      systems = [ "x86_64-linux" "aarch64-linux" ];
-      forSystem = system: let
-        pkgs = import nixpkgs { inherit system; };
-        homeModules = import "${vicinae-src}/modules/home-manager" { inherit pkgs; };
-        
-        # Package builds
-        gitVicinae = pkgs.stdenv.mkDerivation {
-          pname = "vicinae-git";
-          version = "unstable";
-
-          src = inputs.vicinae-src;
-
-          buildInputs = with pkgs; [
-            cmake
-            ninja
-            nodejs_latest
-            kdePackages.qtbase
-            kdePackages.qtsvg
-            gcc_latest
-            protobuf
-            cmark-gfm
-            kdePackages.layer-shell-qt
-            libqalculate
-            minizip-ng
-            kdePackages.qtkeychain
-            rapidfuzz-cpp
-            ccache
-            mold
-          ];
-
-          buildPhase = ''
-            mkdir build
-            cd build
-            cmake .. -DCMAKE_BUILD_TYPE=Release -DLTO=ON -DNOSTRIP=OFF
-            make host-optimized
-          '';
-
-          installPhase = ''
-            mkdir -p $out/bin
-            cp build/vicinae $out/bin/
-          '';
+  outputs = { self, nixpkgs, git, stable, ... }:
+    {
+      packages = {
+        x86_64-linux = {
+          git = git.packages.x86_64-linux.default;
+          latest = git.packages.x86_64-linux.default;
+          stable = stable.packages.x86_64-linux.default;
+          default = git.packages.x86_64-linux.default;
         };
-
-        stableVicinae = pkgs.stdenv.mkDerivation {
-          pname = "vicinae-stable";
-          version = "latest";
-
-          src = pkgs.fetchTarball {
-            url = "https://github.com/vicinaehq/vicinae/releases/latest/download/vicinae.tar.gz";
-            sha256 = "0000000000000000000000000000000000000000000000000000"; # Replace with actual hash
-          };
-
-          buildInputs = with pkgs; [             cmake
-            ninja
-            nodejs_latest
-            kdePackages.qtbase
-            kdePackages.qtsvg
-            gcc_latest
-            protobuf
-            cmark-gfm
-            kdePackages.layer-shell-qt
-            libqalculate
-            minizip-ng
-            kdePackages.qtkeychain
-            rapidfuzz-cpp
-            ccache
-            mold ];
-
-          buildPhase = ''
-            mkdir build
-            cd build
-            cmake .. -DCMAKE_BUILD_TYPE=Release
-            make
-          '';
-
-          installPhase = ''
-            mkdir -p $out/bin
-            cp build/vicinae $out/bin/
-          '';
-        };
-      in
-      {
-        packages = {
-          git = gitVicinae;
-          stable = stableVicinae;
-        };
-
-        homeModules.vicinae = homeModules; # reuse vicinae home-manager module here
       };
 
-    in
-    {
-      # Expose packages under each system
-      packages = builtins.listToAttrs (map (system: {
-        name = system;
-        value = forSystem system;
-      }) systems);
-
-      # Expose home-manager modules
-      homeModules = forSystem systems[0].homeModules; # example: expose for first system
+      defaultPackage.x86_64-linux = self.packages.x86_64-linux.latest;
     };
 }
